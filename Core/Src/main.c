bb6f79c235429fec0e07617961e74e0e5c4313ac
Karukosa <hunghpop123@gmail.com>
2026-05-27 09:40:02 +0700

    Add files via upload

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -350,7 +350,8 @@ static void MX_SPI1_Init(void)
   hspi1.Init.CLKPolarity = SPI_POLARITY_LOW;
   hspi1.Init.CLKPhase = SPI_PHASE_2EDGE;
   hspi1.Init.NSS = SPI_NSS_SOFT;
-  hspi1.Init.BaudRatePrescaler = SPI_BAUDRATEPRESCALER_16;
+  /* SPI1 clock = APB2/32 = 84MHz/32 = 2.625MHz, compatible with MAX31865 (SCLK max 5MHz). */
+  hspi1.Init.BaudRatePrescaler = SPI_BAUDRATEPRESCALER_32;
   hspi1.Init.FirstBit = SPI_FIRSTBIT_MSB;
   hspi1.Init.TIMode = SPI_TIMODE_DISABLE;
   hspi1.Init.CRCCalculation = SPI_CRCCALCULATION_DISABLE;
